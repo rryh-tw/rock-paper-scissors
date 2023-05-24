e5fd1fb689ee655efdd082064c3773872554c2b1
Remi <remi@RemideMacBook-Pro.local>
2023-05-24 12:02:35 +0800

    add QT basic view integrate with live result video

--- main.py
+++ main.py
@@ -3,39 +3,174 @@ import mediapipe as mp
 from mediapipe import tasks
 from mediapipe.tasks.python import vision
 from itertools import count
+import os
+import sys
+import time
 
-def main():
 
-    def callback(result: vision.GestureRecognizerResult, img: mp.Image, timestamp: int):
-        nonlocal text
-        text = f"{timestamp} {result.gestures[0][0].category_name}" if result.gestures else f"{timestamp}"
+from PySide6.QtCore import Qt, QThread, Signal, Slot
+from PySide6.QtGui import QAction, QImage, QKeySequence, QPixmap
 
-    text = ""
+from PySide6.QtWidgets import (QApplication, QComboBox, QGroupBox,
+                               QHBoxLayout, QLabel, QMainWindow, QPushButton,
+                               QSizePolicy, QVBoxLayout, QWidget)
 
-    options = vision.GestureRecognizerOptions(
-        base_options=tasks.BaseOptions(model_asset_path='gesture_recognizer.task'),
-        running_mode=vision.RunningMode.LIVE_STREAM,
-        result_callback=callback)
 
-    cap = cv2.VideoCapture(0)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1080)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1920)
 
-    with vision.GestureRecognizer.create_from_options(options) as recognizer:
-        for timestamp in count(1):
-            ret, frame = cap.read()
-            if ret :
-                frame = cv2.flip(frame, 1)
-                mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
-                print(mp_image)
-                recognizer.recognize_async(mp_image, timestamp)
-                cv2.putText(frame, text, (10, 40), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 255), 1, cv2.LINE_AA)
-                cv2.imshow('Mediapipe Feed', frame)
-            if cv2.waitKey(1) == 27:
-                break
+
+result_text = ""
+def callback(result: vision.GestureRecognizerResult, img: mp.Image, timestamp: int):
+    global result_text
+    result_text = f"{timestamp} {result.gestures[0][0].category_name}" if result.gestures else f"{timestamp}"
+
+
+
+options = vision.GestureRecognizerOptions(
+    base_options=tasks.BaseOptions(model_asset_path='gesture_recognizer.task'),
+    running_mode=vision.RunningMode.LIVE_STREAM,
+    result_callback=callback)
+recognizer = vision.GestureRecognizer.create_from_options(options)
     
-    cap.release()
-    cv2.destroyAllWindows()
 
-if __name__ == '__main__':
-    main()
+
+
+# -------------------------------
+class Thread(QThread):
+    updateFrame = Signal(QImage)
+    def __init__(self, parent=None):
+        QThread.__init__(self, parent)
+        self.trained_file = None
+        self.status = True
+        self.cap = True
+
+    def set_file(self, fname):
+        # The data comes with the 'opencv-python' module
+        self.trained_file = os.path.join(cv2.data.haarcascades, fname)
+
+    def run(self):
+
+        self.cap = cv2.VideoCapture(0)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1080)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1920)
+
+        for timestamp in count(1):
+            ret, frame = self.cap.read()
+            if not ret:
+                continue
+
+            frame = cv2.flip(frame, 1)
+            mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+            print(mp_image)
+            recognizer.recognize_async(mp_image, timestamp)
+            cv2.putText(frame, result_text, (10, 40), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 255), 1, cv2.LINE_AA)
+            # Reading the image in RGB to display it
+            color_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            # Creating and scaling QImage
+            h, w, ch = color_frame.shape
+            img = QImage(color_frame.data, w, h, ch * w, QImage.Format_RGB888)
+            scaled_img = img.scaled(640, 480, Qt.KeepAspectRatio)
+
+            # Emit signal
+            self.updateFrame.emit(scaled_img)
+        sys.exit(-1)
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        # Title and dimensions
+        self.setWindowTitle("Patterns detection")
+        self.setGeometry(0, 0, 800, 500)
+
+        # Main menu bar
+        self.menu = self.menuBar()
+        self.menu_file = self.menu.addMenu("File")
+        exit = QAction("Exit", self, triggered=qApp.quit)
+        self.menu_file.addAction(exit)
+
+        self.menu_about = self.menu.addMenu("&About")
+        about = QAction("About Qt", self, shortcut=QKeySequence(QKeySequence.HelpContents),
+                        triggered=qApp.aboutQt)
+        self.menu_about.addAction(about)
+
+        # Create a label for the display camera
+        self.label = QLabel(self)
+        self.label.setFixedSize(640, 480)
+
+        # Thread in charge of updating the image
+        self.th = Thread(self)
+        self.th.finished.connect(self.close)
+        self.th.updateFrame.connect(self.setImage)
+
+
+        # Buttons layout
+        buttons_layout = QHBoxLayout()
+        self.button1 = QPushButton("Start")
+        self.button2 = QPushButton("Stop/Close")
+        self.button1.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+        self.button2.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+        buttons_layout.addWidget(self.button2)
+        buttons_layout.addWidget(self.button1)
+
+        right_layout = QHBoxLayout()
+        # right_layout.addWidget(self.group_model, 1)
+        right_layout.addLayout(buttons_layout, 1)
+
+        # Main layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addLayout(right_layout)
+
+        # Central widget
+        widget = QWidget(self)
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+        # Connections
+        self.button1.clicked.connect(self.start)
+        self.button2.clicked.connect(self.kill_thread)
+        self.button2.setEnabled(False)
+
+        # Result Text
+        self.text = QLabel("result: ")
+        layout.addWidget(self.text, alignment=Qt.AlignmentFlag.AlignCenter)
+
+    @Slot()
+    def set_model(self, text):
+        self.th.set_file(text)
+
+    @Slot()
+    def kill_thread(self):
+        print("Finishing...")
+        self.button2.setEnabled(False)
+        self.button1.setEnabled(True)
+        self.th.cap.release()
+        cv2.destroyAllWindows()
+        self.status = False
+        self.th.terminate()
+        # Give time for the thread to finish
+        time.sleep(1)
+
+    @Slot()
+    def start(self):
+        print("Starting...")
+        self.button2.setEnabled(True)
+        self.button1.setEnabled(False)
+        # self.th.set_file(self.combobox.currentText())
+        self.th.start()
+
+    @Slot(QImage)
+    def setImage(self, image):
+        self.label.setPixmap(QPixmap.fromImage(image))
+    
+    @Slot(QLabel)
+    def setResultText(self):
+        self.text.setText(result_text)
+
+
+if __name__ == "__main__":
+    app = QApplication()
+    w = Window()
+    w.show()
+    sys.exit(app.exec())
